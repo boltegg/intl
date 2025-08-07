f0428660ff47127df11cec467df11b4b4c613ac0
Eugene Palyan <boltegg@icloud.com>
2025-08-07 16:58:04 +0300

    Update abbreviated name for russian weekdays

--- internal/tst/main.go
+++ internal/tst/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/boltegg/intl"
 	"golang.org/x/text/language"
+
+	"github.com/boltegg/intl"
 )
 
 func main() {
-	v := intl.NewDateTimeFormat(language.Latvian, intl.Options{Year: intl.YearNumeric})
+	v := intl.NewDateTimeFormat(language.English, intl.Options{Minute: intl.Minute2Digit, Hour: intl.Hour2Digit})
+	fmt.Println(v.Format(time.Now()))
 
+	v = intl.NewDateTimeFormatLayout(language.English, "MEd")
 	fmt.Println(v.Format(time.Now()))
 }
