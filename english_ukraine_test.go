40f2c20036fda2660a6e4480cddc34af49b81755
Eugene <boltegg@icloud.com>
2025-08-08 11:53:07 +0300

    Fix English Ukraine MMMd ordering

--- english_ukraine_test.go
+++ english_ukraine_test.go
@@ -45,3 +45,16 @@ func TestDateTimeFormat_EnglishUkraineYMd_NoPadding(t *testing.T) {
 		t.Fatalf("want %q got %q", want, got)
 	}
 }
+
+func TestDateTimeFormat_EnglishUkraineMMMd(t *testing.T) {
+        t.Parallel()
+
+        date := time.Date(2025, time.July, 21, 0, 0, 0, 0, time.UTC)
+        locale := language.MustParse("en-UA")
+
+        got := NewDateTimeFormatLayout(locale, "MMMd").Format(date)
+        want := "21 Jul"
+        if got != want {
+                t.Fatalf("want %q got %q", want, got)
+        }
+}
